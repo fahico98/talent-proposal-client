cf9f76931ec86135e6574d95458c021699a354a5
Fahibram <fahico98@gmail.com>
2021-01-31 18:45:47 -0500

    Home page ready

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,162 @@
 
 <template>
-
    <div class="home">
-      <h1>Home</h1>
-   </div>
 
+      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+         
+         <ol class="carousel-indicators">
+            <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"></li>
+            <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"></li>
+            <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"></li>
+            <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3"></li>
+            <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4"></li>
+         </ol>
+
+         <div class="carousel-inner">
+            <div class="carousel-item active">
+               <img src="@/assets/images/carousel_1.jpg" class="carousel-image d-block">
+            </div>
+            <div class="carousel-item">
+               <img src="@/assets/images/carousel_2.jpg" class="carousel-image d-block">
+            </div>
+            <div class="carousel-item">
+               <img src="@/assets/images/carousel_3.jpg" class="carousel-image d-block">
+            </div>
+            <div class="carousel-item">
+               <img src="@/assets/images/carousel_4.jpg" class="carousel-image d-block">
+            </div>
+            <div class="carousel-item">
+               <img src="@/assets/images/carousel_5.jpg" class="carousel-image d-block">
+            </div>
+         </div>
+
+         <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+         </a>
+         
+         <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+         </a>
+      
+      </div>
+
+      <div class="container my-5 py-5">
+
+         <div class="row">
+            
+            <div class="col-lg-4 px-4">
+               <img src="@/assets/images/picture_1.jpg" class="mx-auto d-block rounded-circle border" height="auto" width="60%">
+               <h4 class="text-center mt-4">Carlos Gutierrez</h4>
+               <p class="text-center mt-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae provident porro, doloremque quae omnis accusamus a non, eum delectus alias pariatur odit saepe! Quaerat, asperiores! Odit laboriosam a vero.</p>
+            </div>
+
+            <div class="col-lg-4 px-4">
+               <img src="@/assets/images/picture_2.jpg" class="mx-auto d-block rounded-circle border" height="auto" width="60%">
+               <h4 class="text-center mt-4">Daniel Medrano</h4>
+               <p class="text-center mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nam sed eos eligendi hic neque sunt ullam necessitatibus ducimus fugiat, ex quibusdam incidunt eaque quos. Corporis odio culpa expedita harum?</p>
+            </div>
+
+            <div class="col-lg-4 px-4">
+               <img src="@/assets/images/picture_3.jpg" class="mx-auto d-block rounded-circle border" height="auto" width="60%">
+               <h4 class="text-center mt-4">Ingrid Sarate</h4>
+               <p class="text-center mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quas, eius laudantium vitae harum earum officiis quibusdam sit fugiat adipisci id iusto libero tempora laboriosam consequatur expedita, quam omnis! Molestias.</p>
+            </div>
+
+         </div>
+
+         <hr style="border-top: 1px solid" class="p-0 mt-5">
+
+         <div class="row mt-5 px-3">
+
+            <div class="col-lg-8">
+               <h1 class="text-color3 mb-3">Primer titulo</h1>
+               <p class="text-color4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, harum, ad placeat deleniti cumque veniam consequuntur unde cupiditate maiores esse animi ullam modi.</p>
+               <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero hic tempore ducimus.</p>
+               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis velit, quia vero aperiam animi nulla, explicabo, expedita necessitatibus optio voluptatem perspiciatis nisi. Ratione, tempore sit! Temporibus excepturi distinctio soluta voluptates?</p>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptates aspernatur, alias dolore nostrum veritatis dolorum ea fugiat accusamus ab atque amet perferendis necessitatibus impedit temporibus quidem eveniet at sit.</p>
+            </div>
+
+            <div class="col-lg-4">
+               <img src="@/assets/images/big_1.jpg" class="mx-auto d-block img-thumbnail" height="auto" width="100%">
+            </div>
+
+         </div>
+
+         <hr style="border-top: 1px solid" class="p-0 m-0 mt-5">
+
+         <div class="row mt-5 px-3">
+
+            <div class="col-lg-4">
+               <img src="@/assets/images/big_2.jpg" class="mx-auto d-block img-thumbnail" height="auto" width="100%">
+            </div>
+
+            <div class="col-lg-8">
+               <h1 class="text-color3 mb-3">Segundo titulo</h1>
+               <p class="text-color4">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam voluptas alias blanditiis libero provident voluptate sed hic, ex, eveniet soluta saepe repellendus sint aspernatur vel voluptatum at a, dicta quas.</p>
+               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. quis odio, maxime in eligendi expedita excepturi recusandae sunt. Modi excepturi nesciunt odio temporibus sapiente culpa? Nulla!</p>
+               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae repellat laboriosam, doloremque libero maiores vitae natus provident amet ad consequatur atque incidunt sed laborum deserunt animi.</p>
+            </div>
+
+         </div>
+
+         <hr style="border-top: 1px solid" class="p-0 m-0 mt-5">
+
+         <div class="row mt-5 px-3">
+
+            <div class="col-lg-8">
+               <h1 class="text-color3 mb-3">Tercer titulo</h1>
+               <p class="text-color4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit quas doloribus nostrum at eaque dicta minus illum commodi? Alias, doloribus? Exercitationem, dicta qui iusto non ut repellat quis similique sint!</p>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, animi officia. Fugit dolor temporibus rem officia fuga doloribus cupiditate corporis, inventore, maiores aut veniam eos. Sed ab excepturi qui fugit?</p>
+               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque facilis cum ex maiores, iusto dolorem voluptates eius, perspiciatis vero voluptate ratione blanditiis laudantium quidem, consequatur debitis harum labore totam est!</p>
+            </div>
+
+            <div class="col-lg-4">
+               <img src="@/assets/images/big_3.jpg" class="mx-auto d-block img-thumbnail" height="auto" width="100%">
+            </div>
+
+         </div>
+
+         <hr style="border-top: 1px solid" class="p-0 m-0 mt-5">
+
+         <div class="row mt-5 px-3">
+
+            <div class="col-lg-4">
+               <img src="@/assets/images/big_4.jpg" class="mx-auto d-block img-thumbnail" height="auto" width="100%">
+            </div>
+
+            <div class="col-lg-8">
+               <h1 class="text-color3 mb-3">Cuarto titulo</h1>
+               <p class="text-color4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente ex officiis, necessitatibus eligendi autem culpa recusandae quibusdam fugit et itaque nulla dicta quis quisquam fuga nemo vitae.</p>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, animi officia. Fugit dolor temporibus rem officia fuga doloribus cupiditate corporis ?</p>
+               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque facilis cum ex maiores, iusto dolorem voluptates eius.</p>
+               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus excepturi fugit harum aspernatur est quam cumque vel qui, repellendus voluptate dolores eius perferendis minus, laudantium, vitae dolorum ut nulla. Expedita.</p>
+            </div>
+
+         </div>
+
+      </div>
+
+   </div>
 </template>
 
 <script>
 
    export default {
-      
-      name: 'home',
-
-      components: {
-         
-      }
+      name: 'home'
    }
 
 </script>
+
+<style scoped>
+
+   .carousel-image{
+      height: 500px;
+      width: 100%;
+      object-fit: cover;
+      object-position: 50% 50%;
+   }
+
+</style>
